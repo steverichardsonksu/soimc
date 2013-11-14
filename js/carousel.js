52c1552d4bcf98f99e656b3734351fd4d9580591
ksuwdc <kyle.kinnaman@ksuwdc.com>
2013-11-13 20:31:11 -0500

    cleaned up carousel JS

--- js/carousel.js
+++ js/carousel.js
@@ -1,14 +1,23 @@
 (function soimcCarousel() {
-	var outer = 1;
+	var nextVisible = 1,
+		runCarousel,
+		carouselVideo = document.getElementsByClassName('carousel_video')[0];
 
 	function startCarousel() {
-		runCarousel = setInterval(function() {moveCarousel(outer);}, 5000);
+		runCarousel = setInterval(function() {
+			moveCarousel(nextVisible);
+		}, 5000);
 	}
 
-	function moveCarousel(inner) {var carousel_items = document.getElementsByClassName('carousel_item');document.getElementsByClassName('visible')[0].classList.remove('visible');outer++;carousel_items[inner].classList.add('visible');if(inner == carousel_items.length - 1) {outer = 0;}}
-
-	carouselVideo = document.getElementsByClassName('carousel_video')[0];
-
+	function moveCarousel(currentlyVisible) {
+		var carousel_items = document.getElementsByClassName('carousel_item');
+		document.getElementsByClassName('visible')[0].classList.remove('visible');
+		nextVisible++;
+		carousel_items[currentlyVisible].classList.add('visible');
+		if(currentlyVisible === carousel_items.length - 1) {
+				nextVisible = 0;
+		}
+	}
 
 	if(carouselVideo) {
 		carouselVideo.addEventListener('play', function() {
@@ -17,31 +26,14 @@
 		});
 
 		carouselVideo.addEventListener('ended', function() {
-			setTimeout(function() {moveCarousel(outer);}, 0);
+			moveCarousel(nextVisible);
 			startCarousel();
 			carouselVideo.load();
 		});
-
-		carouselVideo.addEventListener('mouseover', function() {
-			toggleControls();
-		});
-
-		carouselVideo.addEventListener('mouseout', function() {
-			toggleControls();
-		});
-
-		function toggleControls() {
-			if (carouselVideo.hasAttribute('controls')) {
-				carouselVideo.removeAttribute('controls');
-			} else {
-				carouselVideo.setAttribute('controls','controls');
-			}
-		}
 	}
 
 	function stopCarousel() {
 		clearInterval(runCarousel);
-
 	}
 
 	startCarousel();
